40f857d7ef105b04a33c939d5a7a190815440233
Edison Cruz <editigerun@gmail.com>
2024-05-16 16:13:59 -0500

    third commit

--- src/PartsUnlimited.Models/Manufacturer.cs
+++ src/PartsUnlimited.Models/Manufacturer.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+
+//third change
 using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
